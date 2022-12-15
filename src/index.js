384810300efc0f0dc949aef427a4487ca342cccb
Tusharg376 <honeygautam0@gmail.com>
2022-12-15 21:00:55 +0530

    after project

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://newdatabase:Gd6tycxuRBETdhM7@ourowncluster.jzinjug.mongodb.net/tusharGautam", {
+   
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
