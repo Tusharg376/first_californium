2ee27d9efe40d71143d4c8133fb4777c56f6b515
Tusharg376 <honeygautam0@gmail.com>
2022-12-23 11:44:40 +0530

    after assignmet deleted everything

--- src/models/publisherModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const publisher = new mongoose.Schema({
-    name:String,
-    headQuarter:String
-},{timestamps:true})
-
-module.exports = mongoose.model('publisher', publisher)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const allContollers = require('../controllers/allControllers')
+
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.post('/createAuthor', allContollers.createAuthor)
-router.post('/createPublisher', allContollers.createPublisher)
-router.post('/createBook', allContollers.createBook)
-router.get('/allBooks', allContollers.allBooks)
-router.put('/updateCover', allContollers.updateCover)
-router.put('/updatePrice', allContollers.updatePrice)
+
 module.exports = router;
